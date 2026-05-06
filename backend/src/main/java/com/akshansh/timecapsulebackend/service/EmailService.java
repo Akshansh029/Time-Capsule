67f58ca537a6a0c0c1d32c75c62d57546d3273b3
Akshansh <akshanshsingh00@gmail.com>
2026-05-07 02:00:28 +0530

    Backend: Added verification code generator and verify code email in Email Service

--- backend/src/main/java/com/akshansh/timecapsulebackend/service/EmailService.java
+++ backend/src/main/java/com/akshansh/timecapsulebackend/service/EmailService.java
@@ -6,6 +6,7 @@ import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,22 @@ public class EmailService {
 
     @Value("${app.frontend.url}")
     private String frontendUrl;
+
+    public void sendVerificationEmail(String toEmail, String verificationCode) {
+        try{
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(toEmail);
+            helper.setSubject("Time Capsule: Email Verification Code");
+            helper.setText("Your verification code is: " + verificationCode);
+
+            mailSender.send(message);
+            log.info("Email verification code SENT to: {}", toEmail);
+        } catch(MessagingException e){
+            log.error("Failed to send code verification email to {}: {}", toEmail, e.getMessage());
+        }
+    }
 
     public void sendUnlockNotification(Capsule capsule) {
         List<String> recipients = new ArrayList<>();
