c6d1842c8f05abe34f045f8b9704f51db425bc2e
Akshansh <akshanshsingh00@gmail.com>
2026-05-16 15:46:06 +0530

    Updated LocalDateTime to Instant in ActiveResponseDto

--- backend/src/main/java/com/akshansh/timecapsulebackend/model/dto/ActiveUserResponse.java
+++ backend/src/main/java/com/akshansh/timecapsulebackend/model/dto/ActiveUserResponse.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.UUID;
 
 @Data
@@ -16,5 +16,5 @@ public class ActiveUserResponse {
     private UUID id;
     private String name;
     private String email;
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 }
--- backend/src/main/java/com/akshansh/timecapsulebackend/model/dto/UnlockedCapsuleDto.java
+++ backend/src/main/java/com/akshansh/timecapsulebackend/model/dto/UnlockedCapsuleDto.java
@@ -1,15 +1,8 @@
 package com.akshansh.timecapsulebackend.model.dto;
 
-import com.akshansh.timecapsulebackend.model.entity.CapsuleContent;
-import com.akshansh.timecapsulebackend.model.entity.CapsuleMember;
-import com.akshansh.timecapsulebackend.model.entity.CapsuleStatus;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
-import java.util.UUID;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
